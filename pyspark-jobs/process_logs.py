8e3650cb387d6bd8fb076e31ed9648799d271226
smdas205 <smdas205@gmail.com>
2025-09-06 21:49:46 +0530

    Added files and project is updated till Phase 4.

---
+++ pyspark-jobs/process_logs.py
@@ -0,0 +1,62 @@
+# Program to process logs in data-generator/random.csv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_timestamp
+from pyspark.sql.functions import col, sum, count, to_date, count_distinct
+
+#Initialization of DataFrame
+spark =  SparkSession.builder.appName("Process Logs").getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
+df = spark\
+    .read\
+    .csv("file:///home/smdas/projects/ecommerce-data-pipeline/data-generator/random.csv"\
+         , header = True\
+            , inferSchema = True)
+
+#Cleaning Up Columns
+df.withColumn("timestamp", to_timestamp("timestamp", "yyyy-MM-dd HH:mm:ss"))
+df = df.withColumn("price", col("price").cast("double"))
+df_date = df.withColumn("date", to_date("timestamp"))
+df_date = df_date.withColumnRenamed("date", "event_date")
+
+#Event Based Logs
+df.groupBy("type_of_event")\
+    .agg(count("*").alias("event_count")).\
+    coalesce(1)\
+    .write\
+    .option("header",True)\
+    .option("delimiter", ",")\
+        .mode("overwrite")\
+            .csv("file:///home/smdas/projects/ecommerce-data-pipeline/output/event_count")
+
+#Product Based Logs
+df.groupBy("product_id")\
+    .agg(sum("price").alias("product_revenue"),\
+        count("*").alias("product_event_count"),\
+        count_distinct("user_id").alias("product_user_impressions"))\
+    .orderBy("product_id")\
+    .coalesce(1)\
+    .write\
+    .option("header",True)\
+    .option("delimiter", ",")\
+        .mode("overwrite")\
+            .csv("file:///home/smdas/projects/ecommerce-data-pipeline/output/product_count")
+
+#Date Based Logs
+event_counts = df_date.groupBy("event_date")\
+    .pivot("type_of_event")\
+    .agg(count("*"))\
+    .fillna(0)\
+    .orderBy("event_date")
+
+total_revenue = df_date.groupBy("event_date")\
+    .agg(sum("price").alias("total_revenue"))\
+    .orderBy("event_date")
+
+join_events = event_counts.join(total_revenue, on="event_date", how="left").orderBy("event_date")
+
+join_events.coalesce(1)\
+    .write\
+    .option("header",True)\
+    .option("delimiter", ",")\
+        .mode("overwrite")\
+            .csv("file:///home/smdas/projects/ecommerce-data-pipeline/output/daily_events")
