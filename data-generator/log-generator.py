a6be12252cade76a3ff06de14a42fdabf349aa56
Samirchandra Das <smdas205@gmail.com>
2025-11-20 21:56:10 +0530

    Modified file to generate daily logs

--- data-generator/log-generator.py
+++ data-generator/log-generator.py
@@ -1,43 +1,51 @@
 import random
 import csv
 import uuid
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta, date
 
-start = datetime(2023, 1, 1, 0, 0, 0)
-end = datetime(2025, 7, 22, 23, 59, 59)
-def random_timestamp(start, end):
-    diff = end - start
-    random_seconds = random.randint(0, int(diff.total_seconds()))
-    return start + timedelta(seconds=random_seconds)
-
+yesterday = date.today() - timedelta(days=1)
 event = ["view", "cart", "purchase", "return"]
-data = [["user_id", "type_of_event", "timestamp", "product_id", "price"]]
+data = [["user_id", "type_of_event", "event_timestamp", "product_id", "product_revenue"]]
 products = {
     "prod_00": 2399,
-    "prod_01": 1999,
-    "prod_02": 3399,
-    "prod_03": 4599,
-    "prod_04": 7799,
+    "prod_01": 1499,
+    "prod_02": 3699,
+    "prod_03": 4799,
+    "prod_04": 7199,
     "prod_05": 8099,
-    "prod_06": 899,
-    "prod_07": 5599,
-    "prod_08": 6399,
-    "prod_09": 9999}
+    "prod_06": 1199,
+    "prod_07": 5399,
+    "prod_08": 6299,
+    "prod_09": 9599}
 
-for i in range(10000):
+def events(event_list):
+    weight = [9, 6, 4, 2]
+    event_prob = random.choices(event_list, weights=weight, k=1)
+    selected_events = "".join(event_prob)
+    return selected_events
+
+def date_of_event():
+    start_of_yesterday = datetime.combine(yesterday, time.min)
+    random_sec = random.randint(0, 86399)
+    random_datetime = start_of_yesterday + timedelta(seconds=random_sec)
+    return random_datetime
+
+for i in range(100000):
     random_user_id = uuid.uuid4()
-    random_event = random.choice(event)
-    random_time = random_timestamp(start, end)
+    random_event = events(event)
+    random_time = date_of_event()
     random_prod_id = random.choice(list(products.keys()))
     price = products[random_prod_id]
     if random_event == "return":
-        random_price *= -1
+        random_revenue = price * (-1)
     elif random_event == "view" or random_event == "cart":
-        random_price = 0
+        random_revenue = 0
     else:
-        random_price = price
-    data.append([str(random_user_id), random_event, random_time, random_prod_id, random_price])
+        random_revenue = price
+    data.append([str(random_user_id), random_event, random_time, random_prod_id, random_revenue])
 
-with open('random.csv', mode = 'w', newline='') as file:
+data[1:] = sorted(data[1:], key=lambda x:x[2])
+
+with open(f'/home/hadoop/ecommerce-data-pipeline/logs/log_{yesterday}.csv', mode = 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)
